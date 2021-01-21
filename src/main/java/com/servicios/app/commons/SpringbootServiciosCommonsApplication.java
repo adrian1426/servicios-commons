e6fa5b5740ddf109f3a1e544845a219193d33025
Adrian1426 <adrianhh1426@gmail.com>
2021-01-20 23:41:22 -0600

    deshabilitando configuracion a bd

--- src/main/java/com/servicios/app/commons/SpringbootServiciosCommonsApplication.java
+++ src/main/java/com/servicios/app/commons/SpringbootServiciosCommonsApplication.java
@@ -1,8 +1,12 @@
 package com.servicios.app.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//desabilita la configuracion de conexión  BD, ya que por no lo necesitamos
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServiciosCommonsApplication {
 
 }
